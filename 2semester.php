d95bc30812bc44489370cfd659188547d229c637
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2015-04-16 21:49:03 +0200

    Legger felles ressurser i egen .php

--- 2semester.php
+++ 2semester.php
@@ -86,27 +86,13 @@
       </ul>
     </article>
 
-    <article>
-        <a class="kategori" href="http://instabart.no/"><img src="/img/three115.svg" alt="Tannhjul"><h2>Ressurser</h2></a>
-      <ul>
-        <li class="itl"><a href="https://sats.itea.ntnu.no/sso-wrapper/web/wrapper?target=itslearning">itslearning</a></li>
-        <li class="epost"><a href="https://webmail.stud.ntnu.no/">Studentepost</a></li>
-        <li class="software"><a href="https://software.ntnu.no/">Programvare</a></li>
-        <li class="farm"><a href="https://farm.ntnu.no/RDWeb/Pages/en-US/login.aspx?ReturnUrl=/RDWeb/Pages/en-US/Default.aspx">Programfarm</a></li>
-        <li class="ibok"><a href="https://ibok.no/">iBok.no</a></li>
-      </ul>
-    </article>
-
-    <article>
-        <a class="kategori" href="https://www.nabla.no"><img src="/img/nabla.svg" alt="Nabla" class="desaturate"><h2>Fritid</h2></a>
-      <ul>
-        <li class="fb"><a href="https://www.facebook.com/groups/2248934869/?fref=ts">FYSMATs Facebook-gruppe</a></li>
-        <li class="middag"><a href="https://www.sit.no/en/middag/realfag">Realfagskantina</a></li>
-        <li class="bartebuss"><a href="http://bartebuss.no/">Bartebuss</a></li>
-        <li class="samfundet"><a href="http://whatnow.no/events/kategori/by/trondheim/">Konserter i Trondheim</a></li>
-      </ul>
-    </article>
-
+    <!-- Felles ressurser -->
+    <?php
+       $path = $_SERVER['DOCUMENT_ROOT'];
+       $path .= "/ressurser.php";
+       include_once($path);
+    ?>
+    
   </div>
 
 </body>
