5d2fe3422ff42d2e874d87a2cd2b9739da7027fe
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2015-05-19 14:18:24 +0200

    Legger til nyere eksamenssett i matte 2 på FP

--- 2semester.php
+++ 2semester.php
@@ -53,7 +53,7 @@
         <li class="ov"><a href="https://www.math.ntnu.no/mapleta">Maple T.A.</a></li>
         <li class="lf"><a href="https://www.facebook.com/groups/1595115527375519/"><b>LF</b> til Maple T.A.</a></li>
         <li class="lf"><a href="http://folk.ntnu.no/grevsgar/Matte%201%20og%202/Calculus%20A%20Complete%20Course%206th%20Ed.%20Instructors%20Solution%20Manual.pdf"><b>LF</b> til Adams</a></li>
-        <li class="eks"><a href="https://wiki.math.ntnu.no/tma4105/2012v/gml_eks">Eksamenssett (med <b>LF</b>)</a></li>
+        <li class="eks"><a href="https://wiki.math.ntnu.no/tma4105/2015v/eksamensoppgaver">Eksamenssett (med <b>LF</b>)</a></li>
       </ul>
     </article>
     <article>
