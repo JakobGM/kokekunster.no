c3f59034e84cce935615a48a136e561d3851bf12
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2015-08-21 16:23:03 +0200

    Legger inn mal for 4. semester

---
+++ 4semester.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="platform.js"></script>
+<script src="linktracker.js"></script>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Kokekunster - Lenkeportalen for FYSMATere</title>
+
+<!-- Specifies which css file to use -->
+<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,400italic' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="/style.css">
+
+  <!-- Favicon import -->
+  <?php
+     $path = $_SERVER['DOCUMENT_ROOT'];
+     $path .= "/favicon.php";
+     include_once($path);
+  ?>
+
+  <!-- Analytics, inkluderer </head> og <body> -->
+  <?php
+     $path = $_SERVER['DOCUMENT_ROOT'];
+     $path .= "/analytics.php";
+     include_once($path);
+  ?>
+
+  <!-- The logo and the site name -->
+  <?php
+     $path = $_SERVER['DOCUMENT_ROOT'];
+     $path .= "/header.php";
+     include_once($path);
+  ?>
+
+  <!-- Site navigation -->
+  <?php
+     $path = $_SERVER['DOCUMENT_ROOT'];
+     $path .= "/navbar.php";
+     include_once($path);
+  ?>
+  <script>
+    document.getElementById('3semester').style.fontWeight = 'bold';
+    var arkivLink = document.getElementById('arkiv');
+      arkivLink.setAttribute('href', '/arkiv/3.Semester/')
+  </script>
+
+  <!-- The main content. Consists of images, headlines and paragraphs -->
+  <div class="wrap content">
+
+    <article>
+      <a class="kategori" href="https://wiki.math.ntnu.no/tma4215/2014h/start"><img src="/img/paper101.svg" alt="Kalkulator"><h2>NumMat?</h2></a>
+      <ul>
+        <li class="info"><a href="/ressurser/NumMatInfo.pdf">Om emnet (Støvneng)</a></li>
+        <li class="pensum"><a href="https://wiki.math.ntnu.no/tma4215/2014h/curriculum">Mulig pensum?</a></li>
+        <li class="ov"><a href="https://wiki.math.ntnu.no/tma4215/2014h/project">Eksempel på prosjektoppgave?</a></li>
+        <li class="eks"><a href="https://wiki.math.ntnu.no/tma4215/2014h/assignments#old_exams">Lignende eksamenssett (med <b>LF</b>)?</a></li>
+      </ul>
+    </article>
+
+    <!-- Felles ressurser -->
+    <?php
+       $path = $_SERVER['DOCUMENT_ROOT'];
+       $path .= "/ressurser.php";
+       include_once($path);
+    ?>
+
+  </div>
+
+    </body>
+    </html>
