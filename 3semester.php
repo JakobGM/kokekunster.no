5c93c50c4241c9140c37e360cff5d4eea2b72e28
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2015-08-21 16:43:10 +0200

    Oppdaterer 3. semester siden med korrekte lenker

--- 3semester.php
+++ 3semester.php
@@ -47,17 +47,17 @@
   <!-- The main content. Consists of images, headlines and paragraphs -->
   <div class="wrap content">
     <article>
-      <a class="kategori" href="https://wiki.math.ntnu.no/tma4120/2014h/start"><img src="/img/magnifying-glass40.svg" alt="Graf"><h2>Matte 4K</h2></a>
+      <a class="kategori" href="https://wiki.math.ntnu.no/tma4120/2015h/start"><img src="/img/magnifying-glass40.svg" alt="Graf"><h2>Matte 4K</h2></a>
       <ul>
-        <li class="pensum"><a href="https://wiki.math.ntnu.no/tma4120/2014h/tempoplan">Pensum</a></li>
+        <li class="pensum"><a href="https://wiki.math.ntnu.no/tma4120/2015h/tempoplan">Pensum</a></li>
         <li class="lf"><a href="/arkiv/3.Semester/TMA4120%20-%20Matte%204K/LF%20%28Høst%202014%29/"><b>LF</b> til øvingsoppgavene</a></li>
-        <li class="eks"><a href="https://wiki.math.ntnu.no/tma4120/2014h/eksamensoppgaver">Eksamenssett (med <b>LF</b>)</a></li>
+        <li class="eks"><a href="https://wiki.math.ntnu.no/tma4120/2015h/eksamensoppgaver">Eksamenssett (med <b>LF</b>)</a></li>
       </ul>
     </article>
     <article>
-      <a class="kategori" href="http://www.ivt.ntnu.no/ept/fag/tep4105/"><img src="/img/poor.svg" alt="Kran"><h2>Fluidmekanikk</h2></a>
+      <a class="kategori" href="http://www.ivt.ntnu.no/ept/fag/tep4105/index.html"><img src="/img/poor.svg" alt="Kran"><h2>Fluidmekanikk</h2></a>
       <ul>
-        <li class="pensum"><a href="http://www.ivt.ntnu.no/ept/fag/tep4105/pensum/index.html">Pensum</a></li>
+        <li class="pensum"><a href="http://www.ivt.ntnu.no/ept/fag/tep4105/Generell_beskrivelse_2015.pdf">Pensum</a></li>
         <li class="ov"><a href="http://www.ivt.ntnu.no/ept/fag/tep4105/oevinger/index.html">Øvingsoppgaver</a></li>
         <li class="lf"><a href="/arkiv/3.Semester/TEP4105%20-%20Fluidmekanikk/"><b>LF</b> til øvingsoppgavene</a></li>
         <li class="eks"><a href="http://www.ivt.ntnu.no/ept/fag/tep4105/eksamener/index.html">Eksamenssett (med <b>LF</b>)</a></li>
@@ -65,12 +65,12 @@
     </article>
 
     <article>
-      <a class="kategori" href="https://wiki.math.ntnu.no/tma4215/2014h/start"><img src="/img/paper101.svg" alt="Kalkulator"><h2>NumMat?</h2></a>
+      <a class="kategori" href="http://web.phys.ntnu.no/~asudbo/TFY4165_2015/tfy4165.htm"><img src="/img/hot-drink22.svg" alt="Termos"><h2>Termisk fysikk</h2></a>
       <ul>
-        <li class="info"><a href="/ressurser/NumMatInfo.pdf">Om emnet (Støvneng)</a></li>
-        <li class="pensum"><a href="https://wiki.math.ntnu.no/tma4215/2014h/curriculum">Mulig pensum?</a></li>
-        <li class="ov"><a href="https://wiki.math.ntnu.no/tma4215/2014h/project">Eksempel på prosjektoppgave?</a></li>
-        <li class="eks"><a href="https://wiki.math.ntnu.no/tma4215/2014h/assignments#old_exams">Lignende eksamenssett (med <b>LF</b>)?</a></li>
+        <li class="pensum"><a href="http://web.phys.ntnu.no/~asudbo/TFY4165_2015/framdrift.pdf">Pensum</a></li>
+        <li class="ov"><a href="http://web.phys.ntnu.no/~asudbo/TFY4165_2015/tfy4165.htm#ovinger">Øvingsoppgaver</a></li>
+        <li class="lf"><a href="/arkiv/3.Semester/TFY4165%20-%20Termisk%20Fysikk/LF%20%28V%C3%A5r%202015%29/"><b>LF</b> til øvingsoppgavene</a></li>
+        <li class="eks"><a href="http://home.phys.ntnu.no/instdef/arkiv/eksamen/fy1005/index.html">Eksamenssett (med <b>LF</b>)</a></li>
       </ul>
     </article>
 
