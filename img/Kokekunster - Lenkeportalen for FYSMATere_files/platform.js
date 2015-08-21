30ba6ece0c1004daa312772b68aa924126391bbb
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2015-08-22 00:47:35 +0200

    Enda litt padding fikser

---
+++ img/Kokekunster - Lenkeportalen for FYSMATere_files/platform.js
@@ -0,0 +1,5 @@
+<!doctype html><html><head><title>404 Not Found</title><style>
+body { background-color: #ffffff; color: #000000; }
+h1 { font-family: sans-serif; font-size: 150%; background-color: #9999cc; font-weight: bold; color: #000000; margin-top: 0;}
+</style>
+</head><body><h1>Not Found</h1><p>The requested resource /platform.js was not found on this server.</p></body></html>
