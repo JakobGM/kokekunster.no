3f5fcd364d342c75c015f3520d70404c4763a7b7
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2015-08-21 16:58:27 +0200

    Oppdaterer lenke til student e-post

--- ressurser.php
+++ ressurser.php
@@ -2,7 +2,7 @@
   <a class="kategori" href="http://instabart.no/"><img src="/img/three115.svg" alt="Tannhjul"><h2>Ressurser</h2></a>
   <ul>
     <li class="itl"><a href="https://sats.itea.ntnu.no/sso-wrapper/web/wrapper?target=itslearning">itslearning</a></li>
-    <li class="epost"><a href="https://webmail.stud.ntnu.no/">Studentepost</a></li>
+    <li class="epost"><a href="https://outlook.office365.com/owa/?realm=ntnu.no#path=/mail">Studentepost</a></li>
     <li class="software"><a href="https://software.ntnu.no/">Programvare</a></li>
     <li class="farm"><a href="https://farm.ntnu.no/RDWeb/Pages/en-US/login.aspx?ReturnUrl=/RDWeb/Pages/en-US/Default.aspx">Programfarm</a></li>
     <li class="ibok"><a href="https://ibok.no/">iBok.no</a></li>
