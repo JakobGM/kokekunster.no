44096229d0eb4c6a529b77d15cc4ab0624dd1cc3
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2015-08-31 11:59:10 +0200

    Legger til link til øvingsoppgavene i matte 4k

--- 3semester.php
+++ 3semester.php
@@ -52,6 +52,7 @@
         <li class="video"><a href="https://video.adm.ntnu.no/serier/4fe2d4d3dbe03">Videoforelesninger</a></li>
         <li class="fp"><a href="https://wiki.math.ntnu.no/tma4120/2015h/tempoplan">Framdriftsplan</a></li>
         <li class="formelark"><a href="/ressurser/tma4120h2014_formelark.pdf">Formelark</a></li>
+        <li class="ov"><a href="https://wiki.math.ntnu.no/tma4120/2015h/ovinger">Øvingsoppgaver</a></li>
         <li class="lf"><a href="/arkiv/3.Semester/TMA4120%20-%20Matte%204K/LF%20%28Høst%202014%29/"><b>LF</b> til øvingsoppgavene</a></li>
         <li class="eks"><a href="https://wiki.math.ntnu.no/tma4120/2015h/eksamensoppgaver">Eksamenssett (med <b>LF</b>)</a></li>
       </ul>
