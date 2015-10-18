13b839c9ba1a404066cdd8fc34541b0fe1a04501
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2015-10-19 00:55:41 +0200

    Converting .svg article imgs to white to prevent use of filter: invert, which broke IE

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <header>
   <div class="wrap">
-    <img src="/img/cooking15.svg" alt="logo" class="logo">
+    <img src="/img/cooking15white.svg" alt="logo" class="logo">
     <h1>KOKE<span class="thin">kunster</span></h1>
   </div>
 </header>
