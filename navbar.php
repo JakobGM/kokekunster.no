7414bdd94f27b0e17ded1d66fae1df2a046f51fb
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2015-11-27 14:19:05 +0100

    Changing semester cookie value when selecting semester in navbar

--- navbar.php
+++ navbar.php
@@ -2,9 +2,9 @@
   <div class="wrap">
     <ul>
       <li class="arkiv semestervalg"><a onclick="nyttsemester()" title="Bytt standard semester" href="#"><img class="tannhjul" src="/img/gear39.svg" alt="semestervalg"></a></li>
-      <li><a href="/1semester.php"><span id="1semester">1. Semester</span></a></li>
-      <li><a href="/2semester.php"><span id="2semester">2. Semester</span></a></li>
-      <li><a href="/3semester.php"><span id="3semester">3. Semester</span></a></li>
+      <li><a onclick="semestervalg(1)" href="#"><span id="1semester">1. Semester</span></a></li>
+      <li><a onclick="semestervalg(2)" href="#"><span id="2semester">2. Semester</span></a></li>
+      <li><a onclick="semestervalg(3)" href="#"><span id="3semester">3. Semester</span></a></li>
       <li class="arkiv"><a id="arkiv" href="/arkiv/"><img src="/img/storfile98.svg" alt="arkiv">  Arkiv</a></li>
     </ul>
   </div>
@@ -17,4 +17,14 @@ function nyttsemester() {
   window.location.href = '/index.php'
 }
 
+</script>
+
+<script>
+/**
+Mottar et kall når brukeren velger en semesterfane. Lagres i cookie og redirectes deretter.
+**/
+function semestervalg(valg) {
+  document.cookie="semester=" + valg + "; expires=Thu, 31 Dec 2015 12:00:00 UTC";
+  window.location.href = '/' + valg + 'semester.php';
+}
 </script>
