3fe72dcf2e63bf5f302da310f507b6e45950bc60
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2016-01-06 14:20:02 +0100

    Adding StudentWeb to 'annet'

--- ressurser.php
+++ ressurser.php
@@ -5,7 +5,7 @@
     <li class="epost"><a href="https://outlook.office365.com/owa/?realm=ntnu.no#path=/mail">Studentepost</a></li>
     <li class="wolframalpha"><a href="http://www.wolframalpha.com/">WolframAlpha</a></li>
     <li class="symbolab"><a href="https://www.symbolab.com/">Symbolab</a></li>
-    
+
     <!-- Only adding software on 1st semester page (90% of traffic) -->
     <script>
     if (getCookie("semester") == 1) {
@@ -21,6 +21,7 @@
   <a class="kategori" href="http://www.nabla.no"><img src="/img/nabla_logo.svg" alt="Nabla"><h2>Annet</h2></a>
   <ul>
     <li class="fp"><a onclick="calendarRedirect()" href="#">1024-kalender</a></li>
+    <li class="ntnu"><a href="http://studentweb.ntnu.no">StudentWeb</a></li>
     <li class="fb"><a href="https://www.facebook.com/groups/2248934869/?fref=ts">FYSMATs Facebook-gruppe</a></li>
     <li class="middag"><a href="https://www.sit.no/en/middag/realfag">Realfagskantina</a></li>
     <li class="bartebuss"><a href="http://bartebuss.no/">Bartebuss</a></li>
